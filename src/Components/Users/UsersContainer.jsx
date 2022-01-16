d6f18c2aadd34e9985f3fcb4c16af55396b43802
Artem Wolkov <wolkov.art.leo@yandex.ru>
2022-01-16 23:48:04 +0300

    Lesson 63 DAL, axios.create

--- src/Components/Users/UsersContainer.jsx
+++ src/Components/Users/UsersContainer.jsx
@@ -12,28 +12,25 @@ import {
 import * as axios from "axios";
 import usersPreloader from "../../assets/images/UserLoader.gif";
 import {Preloader} from "../common/Preloader";
+import {usersAPI} from "../../api/api";
 
 class UsersContainer extends React.Component {
   componentDidMount() {
     this.props.toggleIsFetching(true)
-    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`, {
-      withCredentials: true
-    })
-      .then(response => {
-        this.props.setUsers(response.data.items)
+    usersAPI.getUsers(this.props.currentPage, this.props.pageSize)
+      .then(data => {
+        this.props.setUsers(data.items)
         this.props.toggleIsFetching(false)
-        this.props.setTotalUsersCount(response.data.totalCount)
+        this.props.setTotalUsersCount(data.totalCount)
       })
   }
 
   onPageChanged = (pageNumber) => {
     this.props.toggleIsFetching(true)
     this.props.setCurrentPage(pageNumber)
-    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`, {
-      withCredentials: true
-    })
-      .then(response => {
-        this.props.setUsers(response.data.items)
+    usersAPI.getUsers(pageNumber, this.props.pageSize)
+      .then(data => {
+        this.props.setUsers(data.items)
         this.props.toggleIsFetching(false)
       })
   }
---
+++ src/api/api.js
@@ -0,0 +1,18 @@
+import * as axios from "axios";
+
+const instance = axios.create({
+  withCredentials: true,
+  baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+  headers: {
+    'API-KEY': 'ccee2fb7-4271-468b-bff1-b5597f7aa817'
+  }
+})
+
+export const usersAPI = {
+  getUsers(currentPage = 1, pageSize = 10) {
+    return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+      .then(response => {
+        return response.data
+      })
+  }
+}
