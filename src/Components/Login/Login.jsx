effde4746840d4a727bb42dca1e259a471bb10fe
Artem Wolkov <wolkov.art.leo@yandex.ru>
2022-02-03 09:08:11 +0300

    install redux-form

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,5 +1,35 @@
 import React from "react";
+import {reduxForm} from "redux-form";
+
+const LoginForm = (props) => {
+  return (
+    <form action="">
+      <div>
+        <input placeholder={'Login'}/>
+      </div>
+      <div>
+        <input placeholder={'Password'}/>
+      </div>
+      <div>
+        <input type={"checkbox"}/>
+        remember me
+      </div>
+      <div>
+        <button type={"submit"}>
+          Login
+        </button>
+      </div>
+    </form>
+  )
+}
+
+const LoginReduxForm = reduxForm({form: 'login'})(LoginForm)
 
 export const Login = (props) => {
-  return <h1>Login</h1>
+  return (
+    <div>
+      <h1>Login</h1>
+      <loginReduxForm/>
+    </div>
+  )
 }
--- src/redux/store-redux.js
+++ src/redux/store-redux.js
@@ -4,12 +4,14 @@ import profileReducer from "./profileReducer";
 import usersReducer from "./usersReducer";
 import authReducer from "./authReducer";
 import thunk from "redux-thunk";
+import {reduxForm} from "redux-form";
 
 const rootReducer = combineReducers({
     dialogsPage: dialogsReducer,
     profilePage: profileReducer,
     usersPage: usersReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: reduxForm
 })
 
 const store = createStore(rootReducer, applyMiddleware(thunk))
